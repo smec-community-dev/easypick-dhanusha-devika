c650728fb80d33a32f78a07c54e051f32ad8957b
dhanusha <dhanushasuresh2003@gmail.com>
2026-03-20 19:03:38 +0530

    new13

--- easypick/core/urls.py
+++ easypick/core/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -9,5 +10,9 @@ urlpatterns = [
     path('shop/',views.shop_view,name='shop'),
     path('about/',views.about_view,name='about'),
     path('contact/',views.contact_view,name='contact'),
+    path('forgot-password/', auth_views.PasswordResetView.as_view( template_name='core/password_reset.html' ), name='password_reset'), 
+    path('forgot-password/done/', auth_views.PasswordResetDoneView.as_view( template_name='core/password_reset_done.html' ), name='password_reset_done'), 
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view( template_name='core/password_reset_confirm.html' ), name='password_reset_confirm'), 
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view( template_name='core/password_reset_complete.html' ), name='password_reset_complete')
     
 ]
